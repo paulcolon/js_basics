548b4e2c2cb70ba37c9c867695c83330cce59507
paulcolon <pdcolon@gmail.com>
2017-04-21 23:36:33 -0400

    upload files

---
+++ scripts.js
@@ -0,0 +1,4 @@
+alert("first pop-up/alert mesg should appear here, click OK and this 1st pop-up goes away will go away");
+alert("secound pop-up/alert mesg should appear, click OK and it goes away, to reviel the html page");
+document.write("<h2>scripts.js file document.write h2 mesg</h2>");
+document.wrong("<p>I'm practicing 'debugging'.</p>");
